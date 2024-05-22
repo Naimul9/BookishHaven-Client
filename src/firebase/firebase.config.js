8fcbf9cc41e4ba43ae5b8245165e16977af06f01
Naimul9 <naimulakib100@gmail.com>
2024-05-23 04:09:30 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbrKQaIyKKvsx5BjXxUqUHU4RQngpn1nE",
+  authDomain: "assignment-11-def60.firebaseapp.com",
+  projectId: "assignment-11-def60",
+  storageBucket: "assignment-11-def60.appspot.com",
+  messagingSenderId: "485194594455",
+  appId: "1:485194594455:web:0f085a031b3937668f11d2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            {/* navbar */}
+<Navbar/>
+            {/* Outlet */}
+
+            {/* Footer */}
+            
+        </div>
+    );
+};
+
+export default Main;
